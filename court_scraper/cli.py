dff4bf53fd0e1c2d3e3a53af263257128ad48b69
Serdar Tumgoren <zstumgoren@gmail.com>
2020-08-10 15:33:24 -0700

    basic CLI bits to test drive GA scraping

--- court_scraper/cli.py
+++ court_scraper/cli.py
@@ -25,16 +25,32 @@ def main():
     password = configs['password']
     download_dir = configs['download_dir']
     logfile_path = configs['logfile']
-    #base_url = "https://publicrecordsaccess.fultoncountyga.gov/Portal/Home/Dashboard/29"
-    url = "https://publicrecordsaccess.fultoncountyga.gov/Portal/Home/Dashboard/29"
-    search_terms = ['Smith', 'foobar']
+    Path(download_dir).mkdir(parents=True, exist_ok=True)
+    #url = "https://publicrecordsaccess.fultoncountyga.gov/Portal/Home/Dashboard/29"
+    url = "https://ody.dekalbcountyga.gov/portal/Home/Dashboard/29"
+    search_terms = ['Smith']#, 'foobar']
     scraper = OdysseySite(
         url,
         username,
         password,
         download_dir,
     )
-    scraper.search(search_terms, headless=False)
+    data = scraper.search(
+        search_terms,
+        get_detail_page_html=True,
+        headless=False
+    )
+    for row in data:
+        try:
+            html = row.pop('page_source')
+            outfile = Path(download_dir).joinpath(
+                '{}.html'.format(row['case_num'])
+            )
+            with open(outfile, 'w') as fh:
+                fh.write(html)
+        except KeyError:
+            pass
+    print(data)
 
 def cases_to_scrape():
     #Fulton case numbers in 2019 range from 19ED105358 to 19ED150619 -- this includes Jan 1 2020
