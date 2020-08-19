1b0f333624cd6a79ec224401ceb8263c6b0f198b
Serdar Tumgoren <zstumgoren@gmail.com>
2020-08-19 14:24:17 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -45,10 +45,6 @@ setup(
         'License :: OSI Approved :: ISC License (ISCL)',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
     ],
     test_suite='tests',
     tests_require=test_requirements
