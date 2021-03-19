9b7d74e234c786fe8d805364cceff0d333df77c4
Serdar Tumgoren <zstumgoren@gmail.com>
2021-03-19 08:45:20 -0700

    Odyssey refactor (#62)
    * Refactor login strategy for Odyssey #59
    * Update OdysseySite.search to optionally skip case detail scrape #50
    * Add version and setup.cfg

--- tests/platforms/test_odyssey_site.py
+++ tests/platforms/test_odyssey_site.py
@@ -8,16 +8,12 @@ from court_scraper.platforms.odyssey_site import OdysseySite
 @patch('court_scraper.platforms.odyssey_site.site.LoginPage')
 @patch('court_scraper.base.selenium_site.webdriver')
 def test_login(webdriver_mock, login_page_mock):
-    site = OdysseySite(
-        'http://somesite.com',
-        'user',
-        'pass',
-        '/some_path/'
-    )
-    site.login()
-    # Login initializes chrome driver
+    site = OdysseySite('http://somesite.com', '/tmp/some_path/')
+    # Web driver instantiated during Site class initialization
     assert webdriver_mock.Chrome.called
-    # and calls login
+    # Login requires password
+    site.login('user', 'pass')
+    # login method goes to login page and submits user creds
     expected_calls = [
         call().go_to(),
         call().login()
@@ -47,7 +43,34 @@ def test_search(test_input, headless, live_configs, court_scraper_dir):
     username = auth['username']
     password = auth['password']
     url, case_ids = test_input
-    site = OdysseySite(url, username, password, download_dir=court_scraper_dir)
-    site.login(headless=headless)
-    results = site.search(search_terms=case_ids, headless=False)
+    site = OdysseySite(url, download_dir=court_scraper_dir, headless=headless)
+    site.login(username, password)
+    results = site.search(search_terms=case_ids)
     assert len(results) == 1
+    # Scrapes Case Detail page HTML by default
+    assert 'page_source' in results[0].data.keys()
+
+
+@pytest.mark.parametrize(
+    "test_input",
+    [
+        (
+            "https://ody.dekalbcountyga.gov/portal/Home/Dashboard/29",
+            ["19d89169"]
+        ),
+    ]
+)
+@pytest.mark.slow
+@pytest.mark.webtest
+def test_skip_case_details(test_input, headless, live_configs, court_scraper_dir):
+    "should support option to skip scraping of case details"
+    auth = live_configs['ga_dekalb']
+    username = auth['username']
+    password = auth['password']
+    url, case_ids = test_input
+    site = OdysseySite(url, download_dir=court_scraper_dir, headless=headless)
+    site.login(username, password)
+    results = site.search(search_terms=case_ids, case_details=False)
+    # Should *NOT* have case detail HTML stored on return object
+    assert len(results) == 1
+    assert 'page_source' not in results[0].data.keys()
--- tests/test_runner.py
+++ tests/test_runner.py
@@ -26,16 +26,19 @@ def test_site_calls(court_scraper_dir, config_path):
         r.search(search_terms=['foo'])
         username = 'user1@example.com'
         password = 'SECRETPASS'
-        expected_args = [
+        expected_args = (
             'https://ody.dekalbcountyga.gov/portal/Home/Dashboard/29',
-            username,
-            password,
-            court_scraper_dir
-        ]
-        site_class.assert_called_once_with(*expected_args)
+            court_scraper_dir,
+        )
+        expected_kwargs = {'headless': True}
+        # Get the args and kwargs (2nd and 3rd items) from the 
+        # first call which is Site instantiation
+        args, kwargs = site_class.mock_calls[0][1:]
+        assert args == expected_args
+        assert kwargs == expected_kwargs
         login_call, search_call = site_class.mock_calls[1:3]
-        assert login_call == call().login(headless=True)
-        assert search_call == call().search(search_terms=['foo'],headless=True)
+        assert login_call == call().login(username, password)
+        assert search_call == call().search(search_terms=['foo'])
 
 
 @pytest.mark.usefixtures('create_scraper_dir', 'create_config')
