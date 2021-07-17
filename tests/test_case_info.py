56a741d2ec7a2729a28d4a83ee63eeb8f6ec9900
Serdar Tumgoren <zstumgoren@gmail.com>
2021-07-17 01:36:18 -0700

    Refactor CaseInfo and a merge method

--- tests/test_case_info.py
+++ tests/test_case_info.py
@@ -1,6 +1,16 @@
 from court_scraper.case_info import CaseInfo
 
 
+def test_merge():
+    data =  {'foo': 'bar',  'case_num': 1}
+    data2 = {'baz': 'bang', 'case_num': 1}
+    case_info = CaseInfo(data)
+    case_info2 = CaseInfo(data2)
+    case_info.merge(case_info2)
+    assert case_info.case_num == 1
+    assert case_info.foo == 'bar'
+    assert case_info.baz == 'bang'
+
 def test_attribute_mapping():
     mapping = { 'case_num': 'number', }
     data = { 'foo': 'bar', 'case_num': '1' }
