37a56fa53fa6bb2630bcf64be58fb7aa4dce016b
Serdar Tumgoren <zstumgoren@gmail.com>
2021-07-17 18:05:15 -0700

    Create base runner and Odyssey Runner subclass #97

---
+++ court_scraper/platforms/odyssey_site/runner.py
@@ -0,0 +1,49 @@
+import logging
+
+from court_scraper.base.runner import BaseRunner
+
+
+logger = logging.getLogger(__name__)
+
+
+
+class Runner(BaseRunner):
+    """
+    Facade class to simplify invocation and usage of scrapers.
+
+    Arguments:
+
+    - cache_dir -- Path to cache directory for scraped file artifacts (default: {})
+    - config_path -- Path to location of config file
+    - place_id -- Scraper ID made up of state and county (e.g. ga_dekalb)
+
+    """
+
+    def search(self, search_terms=[], headless=True):
+        """
+        For a given scraper, executes the search, acquisition
+        and processing of case info.
+
+        Keyword arguments:
+
+        - search_terms - List of search terms
+        - headless - Whether or not to run headless (default: True)
+
+        Returns: List of dicts containing case metadata
+        """
+        SiteKls = self._get_site_class()
+        url = self.site_meta['home_url']
+        username, password = self._get_login_creds()
+        pos_args = [url]
+        site = SiteKls(
+            *pos_args,
+            self.cache_dir,
+            headless=headless
+        )
+        if username and password:
+            site.login(username, password)
+        logger.info(
+            "Executing search for {}".format(self.place_id)
+        )
+        data = site.search(search_terms=search_terms)
+        return data
