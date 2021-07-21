4338e8ff26af9e0e38c06a513b62b07477b9ee2f
Serdar Tumgoren <zstumgoren@gmail.com>
2021-07-21 14:08:45 -0700

    Add docstrings and remove unused imports

--- court_scraper/captcha/recaptcha.py
+++ court_scraper/captcha/recaptcha.py
@@ -1,6 +1,4 @@
 from anticaptchaofficial.recaptchav2proxyless import *
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 
 class Recaptcha:
 
