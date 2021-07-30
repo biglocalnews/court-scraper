f79c1af8f2e9a0d94a903648f21a821787c11b2b
Serdar Tumgoren <zstumgoren@gmail.com>
2021-07-30 14:42:13 -0700

    Fix Site test

--- tests/test_site.py
+++ tests/test_site.py
@@ -8,6 +8,6 @@ import pytest
     ("ok_tulsa", "Site"),
     ("wi_milwaukee", "Site"),
 ])
-def test_site(place_id, site_class, url):
+def test_site(place_id, site_class):
     site = Site(place_id)
     assert site.__class__.__name__ == site_class
