69547d9f59e8205563bf1f1df8607e86e4499e0f
Serdar Tumgoren <zstumgoren@gmail.com>
2021-07-30 19:39:43 -0700

    Finish initial pass at basic Site facade class

--- court_scraper/site.py
+++ court_scraper/site.py
@@ -12,20 +12,17 @@ class Site:
         meta = cls.get_site_meta(cls, place_id)
         site_type = meta['site_type']
         site_class = cls.get_site_class(cls, place_id, site_type)
+        # Odyssey is super non-standard
         if site_type == 'odyssey':
-            url = meta['home_url']
-            download_dir = cls.get_download_dir(cls, place_id)
-            final_args = [url, download_dir]
+            # Set defaults for omitted kwargs
+            if 'url' not in kwargs:
+                kwargs['url'] = meta['home_url']
+            if 'headless' not in kwargs:
+                kwargs['headless'] = True
+            final_args = [place_id]
         else:
             final_args = args
         return site_class(*final_args, **kwargs)
-
-    def get_download_dir(cls, place_id):
-        try:
-            court_scraper_dir = os.environ['COURT_SCRAPER_DIR']
-        except KeyError:
-            court_scraper_dir = '/tmp'
-        return str(Path(court_scraper_dir, f'cache/{place_id}'))
 
     def get_site_meta(cls, place_id):
         sm = SitesMeta()
