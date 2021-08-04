5ce7403d8f15c0d7e10cf5e5dcc1cc656fff9319
Serdar Tumgoren <zstumgoren@gmail.com>
2021-08-04 14:50:29 -0700

    Restore simple markdown README for PyPI and update setup.py

---
+++ README.md
@@ -0,0 +1,12 @@
+# court-scraper
+
+The `court-scraper` package provides a Python library and command-line
+tool that help search and download case information from county courts
+in the United States.
+
+Learn more or reach out using the below resources:
+
+- Gitter for chat: https://gitter.im/court-scraper/general
+- Documentation: http://court-scraper.readthedocs.io/en/latest/
+- GitHub: https://github.com/biglocalnews/court-scraper
+- PyPI: https://pypi.python.org/pypi/court-scraper
--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import os
 from setuptools import setup, find_packages
+
+
+def read(file_name):
+    this_dir = os.path.dirname(__file__)
+    file_path = os.path.join(this_dir, file_name)
+    with open(file_path) as f:
+        return f.read()
+
 
 requirements = [
     'anticaptchaofficial',
@@ -17,17 +25,20 @@ requirements = [
     'typing-extensions'
 ]
 
+
 test_requirements = [
     'flake8',
     'pytest',
     'pytest-vcr'
 ]
 
+
 setup(
     name='court-scraper',
     version='0.1.0',
-    description="Command-line tool for scraping data from U.S. county courts.",
-    long_description=__doc__,
+    description="Command-line tool for scraping data from U.S. county courts",
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
     author="Serdar Tumgoren",
     author_email='zstumgoren@gmail.com',
     url='https://github.com/biglocalnews/court-scraper',
@@ -47,7 +58,16 @@ setup(
         'License :: OSI Approved :: ISC License (ISCL)',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     test_suite='tests',
-    tests_require=test_requirements
+    tests_require=test_requirements,
+    project_urls={
+        'Maintainer': 'https://github.com/biglocalnews',
+        'Source': 'https://github.com/biglocalnews/court-scraper',
+        'Tracker': 'https://github.com/biglocalnews/court-scraper/issues'
+    },
 )
