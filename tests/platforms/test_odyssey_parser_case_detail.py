3d396f8b4e7cbf7d761d5fd8386d4064be9732fe
Serdar Tumgoren <zstumgoren@gmail.com>
2021-08-26 16:06:04 -0700

    fix test mod for flake8

--- tests/platforms/test_odyssey_parser_case_detail.py
+++ tests/platforms/test_odyssey_parser_case_detail.py
@@ -4,18 +4,17 @@ from court_scraper.platforms.odyssey.parsers.case_detail import CaseDetailParser
 from tests.conftest import read_fixture
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def case_detail_html():
-    return read_fixture(
-        'ga_dekalb/19D67383.html'
-    )
+    return read_fixture("ga_dekalb/19D67383.html")
+
 
 def test_dynamic_attributes(case_detail_html):
     "should extract basic metadata dynamically"
     cdp = CaseDetailParser(case_detail_html)
-    assert cdp.case_number == '19D67383'
-    assert cdp.court == 'Division 0'
-    assert cdp.judicial_officer == 'Anderson, Berryl A'
-    assert cdp.file_date == '01/02/2019'
-    assert cdp.case_type == 'Magistrate Dispossessory - Non Payment of Rent'
-    assert cdp.case_status == 'Closed'
+    assert cdp.case_number == "19D67383"
+    assert cdp.court == "Division 0"
+    assert cdp.judicial_officer == "Anderson, Berryl A"
+    assert cdp.file_date == "01/02/2019"
+    assert cdp.case_type == "Magistrate Dispossessory - Non Payment of Rent"
+    assert cdp.case_status == "Closed"
