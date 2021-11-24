b816a6870c4c7bb4a3a34d0a27b4b9ed9ab0ac32
Justin Mayo <justinmayo@Justins-MacBook-Pro.local>
2021-11-24 14:46:54 -0800

    odyssey pytest updates

--- court_scraper/platforms/odyssey/parsers/case_detail.py
+++ court_scraper/platforms/odyssey/parsers/case_detail.py
@@ -1,4 +1,7 @@
+import re
+
 from lxml import html
+from bs4 import BeautifulSoup
 
 
 class CaseDetailParser:
@@ -6,30 +9,65 @@ class CaseDetailParser:
     def __init__(self, page_source):
         self.page_source = page_source
         self.tree = html.fromstring(page_source)
+        self.soup = BeautifulSoup(page_source, 'html.parser')
 
     def __getattribute__(self, name):
         try:
-            return object.__getattribute__(self, name)
+            return object.__getattribute__(self, name) # e.g. case_number or court or file_date
         except AttributeError:
             titlecase_name = name.replace('_', ' ').title()
             return self._get_text_from_p_tag(titlecase_name)
 
     @property
-    def plaintiffs(self):
-        party_div = self._get_party_div()
-        # TODO: Need to extract plaintiffs
-        plaintiff = party_div.xpath('//*[@id="divPartyInformation_body"]/div[1]/div[1]/p[1]/text()')[1].strip()
-        plaintiff_street = party_div.xpath('//*[@id="divPartyInformation_body"]/div[1]/div[1]/p[2]/text()')[0].strip()
-        plaintiff_city = party_div.xpath('//*[@id="divPartyInformation_body"]/div[1]/div[1]/p[2]/text()')[1].strip()
-        defendant = party_div.xpath('//*[@id="divPartyInformation_body"]/div[3]/div[1]/p[1]/text()')[1].strip()
-        defendant_street = party_div.xpath('//*[@id="divPartyInformation_body"]/div[3]/div[1]/p[2]/text()')[0].strip()
-        defendant_city = party_div.xpath('//*[@id="divPartyInformation_body"]/div[3]/div[1]/p[2]/text()')[1].strip()
-        #breakpoint()
-        return None
+    def parties(self):
+        party_div = self._get_party_div()            
+        party_output = []
+        for span in party_div.find_all(class_=re.compile("tyler-span")):
+            party_dict = {}
+            name_elem = span.find_all("p")[0]
+            if 'Plaintiff' in span.get_text():
+                party_dict['party_type'] = 'Plaintiff'
+                party_dict['party_name'] = ' '.join(name_elem.text.replace('Plaintiff', '').split())
+            if 'Defendant' in span.get_text():            
+                party_dict['party_type'] = 'Defendant'
+                party_dict['party_name'] = ' '.join(name_elem.text.replace('Defendant', '').split())
+            if 'Respondant' in span.get_text():            
+                party_dict['party_type'] = 'Respondant'
+                party_dict['party_name'] = ' '.join(name_elem.text.replace('Respondant', '').split())
+            if 'Address' in span.get_text():
+                address_elem = span.find_all("p")[1]
+                party_dict['address'] = ' '.join(address_elem.text.split())
+            for div in span.find_all("div"):
+                if 'Attorneys' in div.get_text():
+                    attorney_elem = div.next_sibling.next_sibling
+                    if attorney_elem:
+                        party_dict['attorney'] = ' '.join(attorney_elem.text.replace('Retained', '').split())
+            party_output.append(party_dict)                        
+        return party_output
 
     @property
-    def defendants(self):
-        pass
+    def disposition(self):
+        try:
+            judgment_date = self.tree.xpath("//div[@id='dispositionInformationDiv']//text[text() = 'Judgment']/../text()")
+        except:
+            judgment_date = None
+        try:
+            judgment = self.tree.xpath("//div[@id='dispositionInformationDiv']//span[contains(text(), 'Judgment Type')]/../text()[last()]")
+        except:
+            judgment = None
+        try:
+            judgment_for = self.tree.xpath("//div[@id='dispositionInformationDiv']//span[contains(text(), 'Judgment For')]/following-sibling::span/text()")
+        except:
+            judgment_for = None
+        disposition_output = []
+        for i in range(len(judgment_date)):
+            disposition_dict = {}
+            disposition_dict['judgment_date'] = judgment_date[i].strip()
+            disposition_dict['judgment'] = judgment[i].strip()
+            if judgment_for:
+                disposition_dict['judgment_for'] = judgment_for[i].strip()
+            disposition_output.append(disposition_dict)
+        return disposition_output
 
     def _get_text_from_p_tag(self, lookup_text):
         # Select last text node of span's parent p tag
@@ -41,4 +79,7 @@ class CaseDetailParser:
     def _get_party_div(self):
         # Get the Party info div which contains
         # one or more parties
-        return self.tree.cssselect('#divPartyInformation_body')[0]
+        #return self.tree.cssselect('#divPartyInformation_body')[0]
+        return self.soup.select('#divPartyInformation_body')[0]
+
+        
