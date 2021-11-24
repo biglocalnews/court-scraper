23c7260132bf37d9674dd2bbf83ce2bc15f7bfdf
Justin Mayo <justinmayo@Justins-MacBook-Pro.local>
2021-11-24 14:46:54 -0800

    odyssey pytest updates

--- tests/platforms/test_odyssey_parser_case_detail.py
+++ tests/platforms/test_odyssey_parser_case_detail.py
@@ -20,15 +20,121 @@ def test_dynamic_attributes(case_detail_html):
     assert cdp.case_type == 'Magistrate Dispossessory - Non Payment of Rent'
     assert cdp.case_status == 'Closed'
 
-
-def test_plaintiffs(case_detail_html):
-    "should extract plaintiffs"
+def test_parties(case_detail_html):
+    "should extract parties"
     cdp = CaseDetailParser(case_detail_html)
     expected = [
         {
-            'name': 'Johnson, Arthur',
-            'address': '3000 Kelley Chapel RD; Decatur GA 30034',
-            #'attorneys': ['Pro Se'],
+            'party_type': 'Plaintiff', 
+            'party_name': 'Johnson, Arthur', 
+            'address': '3000 Kelley Chapel RD Decatur GA 30034', 
+            'attorney': 'Pro Se'
+        }, 
+        {
+            'party_type': 'Defendant', 
+            'party_name': 'LAKE, JARROD', 
+            'address': '2755 KNOLLIGEN DR DECATUR GA 30034', 
+            'attorney': 'Pro Se'
+        }, 
+        {
+            'party_type': 'Defendant', 
+            'party_name': 'MOORE, PAULA'
         }
     ]
-    assert cdp.plaintiffs == expected
+    assert cdp.parties == expected
+
+def test_disposition(case_detail_html): 
+    "should extract disposition"
+    cdp = CaseDetailParser(case_detail_html)
+    expected = [
+        {
+            'judgment_date': '01/28/2019',
+            'judgment': 'Dismissed with Prejudice'
+        }
+    ]
+    assert cdp.disposition == expected
+
+def test_multiple_dispositions(): 
+    "should extract more than one disposition"
+    html = read_fixture(
+        'ga_dekalb/19D78499.html'
+    )
+    cdp = CaseDetailParser(html)
+    expected = [
+        {
+            'judgment_date': '05/31/2019',
+            'judgment': 'Writ Issued'
+        },
+        {
+            'judgment_date': '06/03/2019',
+            'judgment': 'Dismissed without Prejudice'
+        }
+    ]
+    assert cdp.disposition == expected
+
+def test_party_respondant(): 
+    "should extract parties labelled as respondants"
+    html = read_fixture(
+        'ga_dekalb/21D06499.html'
+    )
+    cdp = CaseDetailParser(html)
+    expected = [
+        {
+            'party_type': 'Respondant', 
+            'party_name': '(Participant) BRANTLEY, CHEKASHA', 
+            'address': '1431 COBB BRANCH DR DECATUR GA 30032'
+        }, 
+        {
+            'party_type': 'Plaintiff', 
+            'party_name': 'owner: VSPATL c/o SYLVAN HOMES LLC', 
+            'address': '3495 PIEDMONT ROAD, BDLG 11, SUTIE 302 ATLANTA GA 30305', 
+            'attorney': 'Lead Attorney Wilson, Lynn M.'
+        }, 
+        {
+            'party_type': 'Defendant', 
+            'party_name': 'OCCUPANTS, UNAUTHORIZED', 
+            'address': '1431 COVV BRANCH DR DECATUR GA 30032'
+        }
+    ]
+    assert cdp.parties == expected
+
+def test_party_no_address(): 
+    "should extract parties with missing address"
+    html = read_fixture(
+        'ga_dekalb/17D27499.html'
+    )
+    cdp = CaseDetailParser(html)
+    expected = [
+        {
+            'party_type': 'Plaintiff', 
+            'party_name': 'OAKS AT STONECREST', 
+            'attorney': 'Lead Attorney Murphy, Andrew T'
+        }, 
+        {
+            'party_type': 'Defendant', 
+            'party_name': 'Hardison, Kenny', 
+            'address': '2795 Evans Mill RD UNIT 2503 Lithonia GA 30058',
+            'attorney': 'Pro Se'
+        },
+        {
+            'party_type': 'Defendant', 
+            'party_name': 'ALL OTHER', 
+            'attorney': 'Pro Se'
+        }
+    ]
+    assert cdp.parties == expected
+
+def test_disposition_judgment_for(case_detail_html): 
+    "should extract dispositions with judgment_for"
+    html = read_fixture(
+        'ga_dekalb/17D22499.html'
+    )
+    cdp = CaseDetailParser(html)
+    expected = [
+        {
+            'judgment_date': '09/14/2017', 
+            'judgment': 'Order and Judgment', 
+            'judgment_for': 'Plaintiff'
+        }
+    ]
+    assert cdp.disposition == expected
