dcc702e7fd6b2deb683c25c43b6c5021ccd37716
Ben Welsh <b@palewi.re>
2022-12-12 20:12:09 +0000

    Some docstrings in sets_meta.py

--- court_scraper/sites_meta.py
+++ court_scraper/sites_meta.py
@@ -1,26 +1,33 @@
+"""Access metadata about the sites we cover."""
 import csv
 import io
 import pkgutil
 
 
 class SitesMeta:
+    """Metadata about the places supported by court-scraper."""
+
     def __init__(self):
         self.data = self._get_sites_data()
 
     def get(self, place_id):
+        """Get metadata about the provided place."""
         state = place_id[:2]
         county = place_id[3:].replace("_", " ")
         key = (state, county)
         return self.data[key]
 
     def get_url(self, state=None, county=None):
+        """Get the home URL of the provided state and county."""
         key = (state, county)
         return self.data[key]["home_url"]
 
     def _get_sites_data(self):
         try:
+            # If the metadata is already in the cache, use that
             return self._data
         except AttributeError:
+            # If we haven't pulled the data before read in the CSV
             text = self._get_sites_csv_text()
             reader = csv.DictReader(io.StringIO(text))
             data = {}
